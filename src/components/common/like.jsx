6943e4475993b479acc395300ffb3b01117dbe3c
Suzy <susan.m.mullin@gmail.com>
2018-12-31 19:21:55 -0800

    added like toggle functionality

---
+++ src/components/common/like.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+//converted class component to stateless functional component as it receives all data it needs via props (doesn't have any state)
+
+//to convert -- remove render, pass in props as parameter, and remove references to it
+
+const Like = (props) => {
+  //empty to full heart when liked/clicked and hand icon when hovered over heart
+  let classes = 'fa fa-heart';
+  if (props.liked) classes += '-o';
+  return (
+    <i
+      onClick={props.onClick}
+      style={{ cursor: 'pointer' }}
+      className={classes}
+      aria-hidden="true"
+    />
+  );
+};
+
+export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService.js';
+import Like from './common/like';
 
 class Movies extends Component {
   state = {
@@ -10,6 +11,19 @@ class Movies extends Component {
     //get all movies except one being deleted
     const movies = this.state.movies.filter((m) => m._id !== movie._id);
     this.setState({ movies: movies });
+  };
+
+  handleLike = (movie) => {
+    //as with Delete, we don't want to modify state directly so we make a copy of all movies here
+    const movies = [...this.state.movies];
+    //find index of the passed object and store it
+    const index = movies.indexOf(movie);
+    //copying all properties here
+    movies[index] = { ...movies[index] };
+    //toggle logic (if true, become false, and vice versa)
+    movies[index].liked = !movies[index].liked;
+    //Passing new movies array to state
+    this.setState({ movies });
   };
 
   render() {
@@ -29,6 +43,7 @@ class Movies extends Component {
               <th>Stock</th>
               <th>Rate</th>
               <th />
+              <th />
             </tr>
           </thead>
           <tbody>
@@ -38,6 +53,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)}
