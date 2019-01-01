bde93d6037c8aab9edce7cea4167d50018d5b7a9
Suzy <susan.m.mullin@gmail.com>
2019-01-01 14:57:41 -0800

    extracted movies table into own component

---
+++ src/components/moviesTable.jsx
@@ -0,0 +1,45 @@
+//moving the movies table into its own component (so that Movies component has same level of abstraction) and passing any objects as props as Movies component should handle the liking and delete
+import React from 'react';
+import Like from './common/like';
+
+const MoviesTable = (props) => {
+  const { movies, onDelete, onLike } = props;
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Stock</th>
+          <th>Rate</th>
+          <th />
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        {movies.map((movie) => (
+          <tr key={movie._id}>
+            <td>{movie.title}</td>
+            <td>{movie.genre.name}</td>
+            <td>{movie.numberInStock}</td>
+            <td>{movie.dailyRentalRate}</td>
+            <td>
+              <Like liked={movie.liked} onClick={() => onLike(movie)} />
+            </td>
+            <td>
+              <button
+                onClick={() => onDelete(movie)}
+                className="btn btn-danger btn-sm"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default MoviesTable;
